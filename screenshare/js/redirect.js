d297e532139d78c99029bf8096eec245e57f62b2
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-06-02 17:58:27 +0800

    change it

--- screenshare/js/redirect.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function(event) {
-	  
-		  var redirect = document.getElementsByName('redirect')[0].value;
-		  window.location.href = redirect;
-		    
-  });
