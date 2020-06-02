d335e20a5ee2a4d5ecca2f1cdb19706e8303945a
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-06-02 18:21:28 +0800

    Update scvideo.js

--- js/scvideo.js
+++ js/scvideo.js
@@ -1,7 +1,7 @@
 
 if(!location.hash.replace('#', '').length) {
-location.href = location.href.split('#')[0] + '#' + (Math.floor(Math.random() * 9999) + 1000).toString().replace('.', '');
-location.reload();
+ location.href = location.href.split('#')[0] + '#' + (Math.random() * 100).toString().replace('.', '');
+ location.reload();
 }
 
 document.createElement('article');
