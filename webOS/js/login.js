32468a51f1c3cb9a47f5577401bf3888c15f887e
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-08-15 12:43:09 +0800

    change

--- webOS/js/login.js
+++ webOS/js/login.js
@@ -20,10 +20,10 @@ function onImageSelected(event) {
 	else
 	{
 		imgtag.title = selectedFile.name;
+		
 	}
 
-	document.getElementById("displayPhoto").width = "250";
-	document.getElementById("displayPhoto").height = "250";
+	fixUserImage();
 
 	reader.onload = function(event) {
 	imgtag.src = event.target.result;
@@ -33,6 +33,14 @@ function onImageSelected(event) {
 	reader.readAsDataURL(selectedFile);
 
 }
+
+function fixUserImage(){
+	
+	document.getElementById("displayPhoto").width = "250";
+	document.getElementById("displayPhoto").height = "250";
+	
+}
+
 function onCheckImage(){
 	var fileName = document.getElementById('userPhoto').value.toLowerCase();
 
@@ -41,20 +49,113 @@ function onCheckImage(){
 	}
 }
 
+function onChangePicDetails(){
+	var chain = sessionStorage.getItem("checkChain");
+	
+	setTimeout(function picTime(){ 
+	
+		if(chain === "false"){
+			var userimage = document.getElementById("displayPhoto").src;
+			localStorage.setItem("userImage", userimage);
+		}
+		
+	}, 60);
+	
+}
+
+
+function onChangeNameDetails(){
+	var chain = sessionStorage.getItem("checkChain");
+	
+	setTimeout(function nameTime(){ 
+	
+		if(chain === "false"){
+			var username = document.getElementById("username").value;
+			localStorage.setItem("userName", username);
+		}
+		
+	}, 60);
+}
+
+function checkChangeTrue(){
+	sessionStorage.setItem("checkChain", "false");
+	sessionStorage.setItem("checkTicked", "true");
+}
+
+function checkChangeFalse(){
+	sessionStorage.setItem("checkChain", "true");
+	sessionStorage.setItem("checkTicked", "false");
+}
+
 function chainCheckbox(){
+	
 	var chain = sessionStorage.getItem("checkChain");
 	
 	if(chain === "true"){
 		document.getElementById("saveDetails").checked = true;
-		sessionStorage.setItem("checkChain", "false");
-	}
-	else if(chain === "false"){
-		document.getElementById("saveDetails").checked = false;
-		sessionStorage.setItem("checkChain", "true");
+		checkChangeTrue();
+		saveDetails();
 	}
 	else{
-		sessionStorage.setItem("checkChain", "true");
-		document.getElementById("saveDetails").checked = true;
+		document.getElementById("saveDetails").checked = false;
+		checkChangeFalse();
+		unloadDetails();
 	}
 
 }
+
+function setLoginStatus(){
+	
+	var chain = sessionStorage.getItem("checkChain");
+	
+	if(chain === "true"){
+		document.getElementById("saveDetails").checked = true;
+		checkChangeTrue();
+		saveDetails();
+	}
+	else{
+		document.getElementById("saveDetails").checked = false;
+		checkChangeFalse();
+		unloadDetails();
+	}
+	
+}
+
+function saveDetails(){
+	var username = document.getElementById("username").value;
+	var userimage = document.getElementById("displayPhoto").src;
+	localStorage.setItem("userName", username);
+	localStorage.setItem("userImage", userimage);
+	localStorage.setItem("userSave", "true");
+}
+
+function unloadDetails(){
+	localStorage.removeItem("userName");
+	localStorage.removeItem("userImage");
+	localStorage.setItem("userSave", "false");
+}
+
+window.onload = function(){
+	if(usersave !== "true"){
+		checkChangeFalse();
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function loadDetails(){
+    var usersave = localStorage.getItem("userSave");
+	
+	if(usersave === "true"){
+		var username = localStorage.getItem("userName");
+		var userimage = localStorage.getItem("userImage");
+		
+		document.getElementById("username").value = username;
+		document.getElementById("displayPhoto").src = userimage;
+		
+		fixUserImage();
+		document.getElementById("saveDetails").checked = true;
+		checkChangeTrue();
+		
+	}
+	
+	
+});
