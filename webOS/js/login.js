7653014541c90f881ef19392b97e78d4d97a904c
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-08-15 08:26:32 +0800

    s

--- webOS/js/login.js
+++ webOS/js/login.js
@@ -1,43 +1,44 @@
+
 function imageSelect(){
-var getPhoto = document.getElementById("userPhoto");
-getPhoto.click();
+	var getPhoto = document.getElementById("userPhoto");
+	getPhoto.click();
 }
 
 function onImageSelected(event) {
-var selectedFile = event.target.files[0];
+	var selectedFile = event.target.files[0];
 
-var reader = new FileReader();
+	var reader = new FileReader();
 
-var imgtag = document.getElementById("displayPhoto");
+	var imgtag = document.getElementById("displayPhoto");
 
-var fileName = document.getElementById('userPhoto').value.toLowerCase();
-if(!fileName.endsWith('.jpg') && !fileName.endsWith('.png') && !fileName.endsWith('.gif') && !fileName.endsWith('.svg')){
-	imgtag.title = "Select an image!";
-	document.getElementById("displayPhoto").src = "https://playdices.github.io/images/profile.png";
-	return false;
-}
-else
-{
-	imgtag.title = selectedFile.name;
-}
+	var fileName = document.getElementById('userPhoto').value.toLowerCase();
+	if(!fileName.endsWith('.jpg') && !fileName.endsWith('.png') && !fileName.endsWith('.gif') && !fileName.endsWith('.svg')){
+		imgtag.title = "Select an image!";
+		document.getElementById("displayPhoto").src = "https://playdices.github.io/images/profile.png";
+		return false;
+	}
+	else
+	{
+		imgtag.title = selectedFile.name;
+	}
 
-document.getElementById("displayPhoto").width = "250";
-document.getElementById("displayPhoto").height = "250";
+	document.getElementById("displayPhoto").width = "250";
+	document.getElementById("displayPhoto").height = "250";
 
-reader.onload = function(event) {
-imgtag.src = event.target.result;
+	reader.onload = function(event) {
+	imgtag.src = event.target.result;
 
-};
+	};
 
-reader.readAsDataURL(selectedFile);
-
-
+	reader.readAsDataURL(selectedFile);
 
 }
 function onCheckImage(){
-var fileName = document.getElementById('userPhoto').value.toLowerCase();
+	var fileName = document.getElementById('userPhoto').value.toLowerCase();
 
-if(document.getElementById("userPhoto").value == ""){
-	document.getElementById("displayPhoto").src = "https://playdices.github.io/images/profile.png";
+	if(document.getElementById("userPhoto").value == ""){
+		document.getElementById("displayPhoto").src = "https://playdices.github.io/images/profile.png";
+	}
 }
-}
+
+
