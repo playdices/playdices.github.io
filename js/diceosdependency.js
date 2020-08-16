65792365fbb077c233cc9cce6ed9c361b9c08b92
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-08-16 11:51:02 +0800

    Update diceosdependency.js

--- js/diceosdependency.js
+++ js/diceosdependency.js
@@ -18,7 +18,15 @@ window.addEventListener('resize', function(){
 });
 
 
-
+document.addEventListener("DOMContentLoaded", function loadDetails(){
+    
+    setTimeout(function vidTime(){ 
+	
+		initializeOS();
+		
+	}, 60);
+	
+});
 
 //LOAD DICE OS
 function initializeOS(){
