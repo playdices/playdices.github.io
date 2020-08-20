660b08608934596bf6463fd957f4caad65fedd94
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-08-20 15:02:47 +0800

    a

--- js/slider.js
+++ js/slider.js
@@ -12,21 +12,26 @@ var dialogIO = localStorage['checkDialog'];
 
 
 $(document).on({
-    'mousemove': function(e) {
-        clicked && updateScrollPos(e);
-    },
-    'mousedown': function(e) {
-        e.preventDefault();        
-        clicked = true;
-        clickX = e.pageX;
-    },
-    'mouseup': function() {
-        clicked = false;
-        $('html').css('cursor', 'auto');
-    }
+	if(dialogIO == "open")
+		'mousemove': function(e) {
+			clicked && updateScrollPos(e);
+		},
+		'mousedown': function(e) {
+			e.preventDefault();        
+			clicked = true;
+			clickX = e.pageX;
+		},
+		'mouseup': function() {
+			clicked = false;
+			$('html').css('cursor', 'auto');
+		}
+	}
+	
 });
 
 var updateScrollPos = function(e) {
-    $('html').css('cursor', 'grabbing');
-    $(window).scrollLeft($(window).scrollLeft() + (clickX - e.pageX));
+	if(dialogIO == "open"){
+		$('html').css('cursor', 'grabbing');
+		$(window).scrollLeft($(window).scrollLeft() + (clickX - e.pageX));
+	}
 }
