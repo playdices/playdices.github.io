08fb1f3f7c62dd0ac0b0f4a0d6ae8047cf1f7ed6
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-08-21 10:54:36 +0800

    a

--- js/dated.js
+++ js/dated.js
@@ -1,15 +1,16 @@
-function date() 
+function dateSet() 
 {
+
    var now = new Date();
    var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
    var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
    var date = ((now.getDate() < 10) ? "0" : "") + now.getDate();
    var year = (now.getYear() < 1000) ? now.getYear() + 1900 : now.getYear();
    today = days[now.getDay()] + ", " + months[now.getMonth()] + " " + date + ", " + year;
+   var basicdate = document.getElementById('basicdate');
+   basicdate.innerHTML = today;
+
    
-   var dateRaw = document.getElementById("dateOP").value;
-   dateRaw = today;
-   
-   setTimeout("date()", 1000);
+   setTimeout("dateSet()", 1000);
 }
-date();
+dateSet();
