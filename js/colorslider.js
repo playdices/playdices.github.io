a41cab43e5b8b20e9969b31f085e529dafdbe20e
Jerwin Dela Cruz <jerwinkdc@gmail.com>
2020-08-23 10:02:31 +0800

    Update colorslider.js

--- js/colorslider.js
+++ js/colorslider.js
@@ -33,7 +33,7 @@ var divB = document.getElementById("thumbie");
 	    $('.canvasFilter').css('filter',thisOffsetColor);
 		
 		ShowObjectWithEffect('canvasFBG', 1, 'fade', 500, 'easeOutExpo');
-		ShowObjectWithEffect('vidContainer', 0, 'fade', 500, 'easeOutExpo');
+		ShowObjectWithEffect('bgDICE', 0, 'fade', 500, 'easeOutExpo');
 		
 		
 	  }
